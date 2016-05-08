1e3296e1a67b9e04df95b8ab0fc0c1a0d8cf60e0
Sadegh Afshari <sa.koorosh@gmail.com>
2016-05-08 16:19:55 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,32 +232,10 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {77076695},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
+    about_text = [[TeleSword v1
 An advanced administration bot based on TG-CLI written in Lua
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
-
-Our website 
-http://teleseed.seedteam.org/
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -471,7 +449,7 @@ will return group ban list
 *Only owner can use res,setowner,promote,demote and log commands
 
 ]],
-	help_text_super =[[
+       help_text_super =[[
 SuperGroup Commands:
 
 !info
@@ -619,7 +597,7 @@ Returns group logs
 end
 
 function on_our_id (id)
-  our_id = id
+  our_id = 0
 end
 
 function on_user_update (user, what)
